a60835719254a240ed77d574dce541967ba20306
Naviya-C <naviyac491@gmail.com>
2025-12-19 09:56:05 +0530

    Add functions to retrieve cars by fuel type and available cars; create __init__.py files for app structure

--- app/repositories/car_repo.py
+++ app/repositories/car_repo.py
@@ -1,4 +1,4 @@
-from ..core.database import supabase_client
+from app.core.database import supabase_client
 
 def get_cars_under_price(max_price: float):
     response = (
@@ -10,3 +10,25 @@ def get_cars_under_price(max_price: float):
         .execute()
     )
     return response.data
+
+def get_cars_by_fuel_type(fuel_type: str):
+    response = (
+        supabase_client
+        .table("cars")
+        .select("brand, model, price, fuel_type")
+        .eq("fuel_type", fuel_type)
+        .eq("availability", True)
+        .execute()
+    )
+    return response.data
+
+def get_available_cars():
+    response = (
+        supabase_client
+        .table("cars")
+        .select("brand, model, price, fuel_type")
+        .eq("availability", True)
+        .execute()
+    )
+    return response.data
+

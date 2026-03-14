d94f5fca8289ac957345ba9a29beabc1efb79e0b
Naveen <naviyac491@gmail.com>
2026-03-14 17:11:21 +0530

    add middleware

--- app/main.py
+++ app/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1.chat import router as chat_router
 
 app = FastAPI(
     title="Car RAG Chatbot",
     version="1.0.0"
+)
+
+# Allow frontend access
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # later replace with your frontend domain
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # Register routes
